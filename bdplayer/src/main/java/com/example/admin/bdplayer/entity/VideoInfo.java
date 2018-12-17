46b01570579901bbb066c1e577c5cf98f101d6ef
Amon <walixiwa@qq.com>
2018-12-17 16:34:05 +0800

    Update VideoInfo.java

--- bdplayer/src/main/java/com/example/admin/bdplayer/entity/VideoInfo.java
+++ bdplayer/src/main/java/com/example/admin/bdplayer/entity/VideoInfo.java
@@ -1,48 +1,11 @@
 package com.example.admin.bdplayer.entity;
 
-import java.io.Serializable;
-
-public class VideoInfo implements Serializable {
-    public String getTitle() {
-        return Title;
-    }
-
-    public void setTitle(String title) {
-        Title = title;
-    }
-
-    public String getUrl() {
-        return Url;
-    }
-
-    public void setUrl(String url) {
-        Url = url;
-    }
-
-    public String getAK() {
-        return AK;
-    }
-
-    public void setAK(String AK) {
-        this.AK = AK;
-    }
-
-    public String Title;
-    public String Url;
-    public String AK;
-}
-package com.example.admin.bdplayer.entity;
-
 import android.content.Context;
-import android.support.annotation.NonNull;
-
 import com.baidu.cloud.media.player.BDCloudMediaPlayer;
 import com.example.admin.bdplayer.BDCloudVideoView;
 import com.example.admin.bdplayer.util.SharedPreferencesBdplayer;
 
-import java.io.Serializable;
-
-public class VideoInfo implements Serializable {
+public class VideoInfo {
     private String Title;
     private String Url;
     private String AK;
